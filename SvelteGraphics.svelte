443b41ceefde641ee92dbd1b0736081af64d038a
mohadel1990 <mohadel1990@gmail.com>
2021-09-22 13:54:14 +0300

    Everything is reactive

--- SvelteGraphics.svelte
+++ SvelteGraphics.svelte
@@ -1,71 +1,59 @@
 <script>
-  export let SvelteGraphic;
-  export let SvelteGraphicSpecs = {
-    lib: ``,
-    type: ``,
-  };
+	export let SvelteGraphic;
+	export let SvelteGraphicSpecs = {
+		lib: ``,
+		type: ``
+	};
 
-  let graphic;
+	let graphic;
 
-  let lib;
-  let type;
-  let size;
-  let width;
-  let height;
-  let color;
-  let color1;
-  let color2;
-  let color3;
-  let modules;
-  let path;
+	let lib;
+	let type;
+	let size;
+	let width;
+	let height;
+	let color;
+	let color1;
+	let color2;
+	let color3;
+	let modules;
+	let path;
 
-  $: graphic = SvelteGraphic;
+	$: graphic = SvelteGraphic;
+	$: type = SvelteGraphicSpecs.type;
+	$: lib = SvelteGraphicSpecs.lib;
 
-  type = SvelteGraphicSpecs.type;
-  lib = SvelteGraphicSpecs.lib;
+	$: switch (type) {
+		case `icon`:
+			type = 'icons';
+			size = SvelteGraphicSpecs.size ? SvelteGraphicSpecs.size : `1em`;
+			break;
+		case `illus`:
+			type = 'illustrations';
+			size = SvelteGraphicSpecs.size ? SvelteGraphicSpecs.size : `500`;
+			break;
+		case `icons`:
+			size = SvelteGraphicSpecs.size ? SvelteGraphicSpecs.size : `1em`;
+			break;
+		case `illustrations`:
+			size = SvelteGraphicSpecs.size ? SvelteGraphicSpecs.size : `500`;
+			break;
 
-  switch (type) {
-    case `icon`:
-      type = "icons";
-      size = SvelteGraphicSpecs.size ? SvelteGraphicSpecs.size : `1em`;
-      break;
-    case `illus`:
-      type = "illustrations";
-      size = SvelteGraphicSpecs.size ? SvelteGraphicSpecs.size : `500`;
-      break;
-    case `icons`:
-      size = SvelteGraphicSpecs.size ? SvelteGraphicSpecs.size : `1em`;
-      break;
-    case `illustrations`:
-      size = SvelteGraphicSpecs.size ? SvelteGraphicSpecs.size : `500`;
-      break;
+		default:
+			throw `Invalid Graphic type: ${type}`;
+	}
 
-    default:
-      throw `Invalid Graphic type: ${type}`;
-  }
+	width = SvelteGraphicSpecs.width ? SvelteGraphicSpecs.width : size;
+	height = SvelteGraphicSpecs.height ? SvelteGraphicSpecs.height : size;
 
-  width = SvelteGraphicSpecs.width ? SvelteGraphicSpecs.width : size;
-  height = SvelteGraphicSpecs.height ? SvelteGraphicSpecs.height : size;
+	$: color = SvelteGraphicSpecs.color ? SvelteGraphicSpecs.color : `currentColor`;
+	$: color1 = SvelteGraphicSpecs.color1 ? SvelteGraphicSpecs.color1 : color;
+	$: color2 = SvelteGraphicSpecs.color2 ? SvelteGraphicSpecs.color2 : color;
+	$: color3 = SvelteGraphicSpecs.color3 ? SvelteGraphicSpecs.color3 : color;
+	//let viewBox = SvelteGraphicSpecs.viewBox ? SvelteGraphicSpecs.viewBox : `0 0 24 24`;
 
-  $: color = SvelteGraphicSpecs.color
-    ? SvelteGraphicSpecs.color
-    : `currentColor`;
-  $: color1 = SvelteGraphicSpecs.color1 ? SvelteGraphicSpecs.color1 : color;
-  $: color2 = SvelteGraphicSpecs.color2 ? SvelteGraphicSpecs.color2 : color;
-  $: color3 = SvelteGraphicSpecs.color3 ? SvelteGraphicSpecs.color3 : color;
-  //let viewBox = SvelteGraphicSpecs.viewBox ? SvelteGraphicSpecs.viewBox : `0 0 24 24`;
-
-  $: path = `./${type}/${lib}/${graphic}.svelte`;
-  $: import(/* @vite-ignore */ path).then((res) => (modules = res.default));
+	$: path = `./${type}/${lib}/${graphic}.svelte`;
+	$: import(/* @vite-ignore */ path).then((res) => (modules = res.default));
 </script>
 
-<svelte:component
-  this="{modules}"
-  color="{color}"
-  color1="{color1}"
-  color2="{color2}"
-  color3="{color3}"
-  size="{size}"
-  width="{width}"
-  height="{height}"
-/>
+<svelte:component this={modules} {color} {color1} {color2} {color3} {size} {width} {height} />
